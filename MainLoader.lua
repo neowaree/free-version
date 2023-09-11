6aef674c729c9bf91fa75aa5b08e08470bd5d00c
neoware <129959811+neowaree@users.noreply.github.com>
2023-09-11 09:46:04 +0000

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -46,7 +46,6 @@ if true then
     game:GetService("Players").LocalPlayer.Idled:Connect(function()
         game:GetService("VirtualUser"):ClickButton2(Vector2.new())
     end)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/neowaree/free-version/main/Assets/114209385120.lua", true))() -- \\ Anti-Cheat
     loadstring(game:HttpGet("https://raw.githubusercontent.com/neowaree/free-version/main/CustomModules/" .. tostring(game.PlaceId) .. ".lua", true))() -- \\ Main Loader
 else
     game.Players.LocalPlayer:Kick("Game Isn't Supported. Request The Game At https://discord.gg/mMBVhq4gnZ")
