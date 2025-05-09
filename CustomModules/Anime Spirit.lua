c4f4f0a4f9e8eb467886505944e6c1cf950e0a29
neoware <129959811+neowaree@users.noreply.github.com>
2025-05-10 00:18:42 +0700

    Anime Spirits.lua

---
+++ CustomModules/Anime Spirit.lua
@@ -0,0 +1,9 @@
+local placeId = game.PlaceId
+
+if placeId == 15527951011 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/neowaree/free-version/refs/heads/main/Anime%20Spirit%20Dungeon"))()
+elseif placeId == 73417524077325 or placeId == 16041086429 or placeId == 11756036029 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/neowaree/free-version/refs/heads/main/Anime%20Spirit"))()
+else
+    warn("This place ID is not recognized by the script.")
+end
