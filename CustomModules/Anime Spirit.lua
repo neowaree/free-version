4f5aca910f6b73705f1a2a2e78853cf2feca703e
neoware <129959811+neowaree@users.noreply.github.com>
2025-05-10 00:23:47 +0700

    Anime Spirit.lua

--- CustomModules/Anime Spirit.lua
+++ CustomModules/Anime Spirit.lua
@@ -1,6 +1,6 @@
 local placeId = game.PlaceId
 
-if placeId == 15527951011 then
+if placeId == 15527951011 or pladeId == 15728325012 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/neowaree/free-version/refs/heads/main/Anime%20Spirit%20Dungeon"))()
 elseif placeId == 73417524077325 or placeId == 16041086429 or placeId == 11756036029 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/neowaree/free-version/refs/heads/main/Anime%20Spirit"))()
