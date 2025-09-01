68c9f144d74bc74e27030b284fad2564eac1138d
neoware <129959811+neowaree@users.noreply.github.com>
2025-09-01 11:39:48 +0700

    73417524077325.lua

--- CustomModules/Anime Spirit.lua
+++ CustomModules/Anime Spirit.lua
@@ -4,7 +4,7 @@ local mapName = game:GetService("MarketplaceService"):GetProductInfo(placeId).Na
 if placeId == 15527951011 or placeId == 15728325012 or mapName == "Anime Spirits Dungeon" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/neowaree/free-version/refs/heads/main/Anime%20Spirit%20Dungeon"))()
 elseif placeId == 73417524077325 or placeId == 16041086429 or placeId == 11756036029 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/neowaree/free-version/refs/heads/main/Anime%20Spirit"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/neowaree/free-version/refs/heads/main/Anime%20Spirit"))()
 else
     warn("This place ID is not recognized by the script.")
 end ---test
